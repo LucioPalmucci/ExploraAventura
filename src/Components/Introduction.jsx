e85a6bb8139ac72e858fc970b6e7a907f661c2ea
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-11-20 12:29:44 -0300

    Header

---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,9 @@
+
+function Introduction(){
+    <div className="w-100">
+        <h4>Un viaje de diversión y conexión con los niños</h4>
+        <h2>Un juego para aprender de mindfulness y regulación emocional</h2>
+    </div>
+
+}
+export default Introduction;
