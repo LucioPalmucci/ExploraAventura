fed9fcab1be8e0b5bf955b4a13170172aaac2366
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-11-20 12:24:51 -0300

    Header + font

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <div id="Encabezado" className="w-full flex items-center justify-center space-x-72 m-5 ml-1 font-Nunito">
+            <img id="Logo" width="294" height={23} src="https://exploraventura.com.ar/wp-content/uploads/2024/05/cropped-EXPLORAVENTURA.png" className=""/>
+            <div className="flex space-x-10 ">
+                <h5>Home</h5>
+                <h5>Regulación Emocional</h5>
+                <h5>Mindfulness</h5>
+                <h5>Aprender</h5>
+            </div>
+            <button className="text-lg p-2 pl-5 pr-5 rounded-3xl bg-red-500 text-white">¡Juega Ahora!</button>
+        </div>
+    )
+}
+
+export default Header;
