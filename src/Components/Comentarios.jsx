17d3253a53269ce211b175cc64854606c84e85fe
LucioPalmucci <lucio.palmucci@hotmail.com>
2024-11-23 20:36:22 -0300

    Comentarios terminados

--- src/Components/Comentarios.jsx
+++ src/Components/Comentarios.jsx
@@ -1,82 +1,86 @@
-import React, { useState } from 'react';
-function Comentarios() {
-    const [currentIndex, setCurrentIndex] = useState(0);
+import React from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+const Comentarios = () => {
 
-    const comentarios = [
-        {
-            id: 1,
-            quote: "La recomiendo!",
-            text: "Me encantó la propuesta , la recomiendo. Ideal para usarlo en casa con la familia, y como herramienta en la parte educativa...",
-            author: "Paola Rossi",
-            source: "mediante Play Store"
-        },
-        {
-            id: 2,
-            quote: "Una herramienta muy valiosa",
-            text: "Excelente juego, super recomendable!!! como padres y madres es una guía bárbara para trabajar mindfulness con nuestros hijos…",
-            author: "Maria Noel Martínez",
-            source: "mediante Play Store"
-        },
-        {
-            id: 3,
-            quote: "Muy Buena!",
-            text: "Es un buena oportunidad de que niños (y adultos también) aprendan a darse un tiempo para escucharse y 'leer' cómo se sienten…",
-            author: "Belen Cacere",
-            source: "mediante Play Store"
-        },
-        {
-            id: 4,
-            quote: "Exelente!",
-            text: "Excelente la app! Me encontré con muchos recursos para conocer más a mi hija y herramientas para ella…",
-            author: "Rosario Moraco",
-            source: "mediante Play Store"
+    const Settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dotsClass: "slick-dots custom-dots bg-red-500 ",
+        customPaging: function (i) {
+            return (
+                <div>
+                    <i className="fas fa-circle text-dots text-fondo"></i>
+                </div>
+            );
         }
-    ];
-
-    const handleDotClick = (index) => {
-        setCurrentIndex(index);
-    };
-
-    const getComentariosToShow = () => {
-        if (currentIndex === comentarios.length) {
-            return comentarios.slice(currentIndex, comentarios[0]);
-        }
-        return comentarios.slice(currentIndex, currentIndex + 2);
-    };
+    }
 
     return (
-        <div className="justify-center aling-center font-Nunito w-full bg-red-500">
-            <div className="text-center w-full p-10">
+        <div className="flex flex-col items-center font-Nunito w-full bg-red-500">
+            <div className="text-center w-full p-8">
                 <h3 className="text-white text-4xl font-bold">Testimonios</h3>
             </div>
-            <div className="justify-center aling-center flex">
-                {getComentariosToShow().map((comentario) => (
-                    <div key={comentario.id} className="bg-white p-5 m-5 rounded-3xl border-l-8 border-sky-200">
+            <div className="w-5/6">
+                <Slider {...Settings} className='flex justify-center items-center'>
+                    <div className="comentario bg-white p-5 m-5 rounded-3xl border-l-8 border-sky-200 mx-4">
                         <i className="fas fa-quote-right text-gray-300 text-4xl"></i>
-                        <h4 className="text-2xl mt-2">{comentario.quote}</h4>
-                        <p className="text-base text-gray-500 italic pt-2 pb-2">{comentario.text}</p>
+                        <h4 className="text-2xl mt-2">La recomiendo!</h4>
+                        <p className="text-base text-gray-500 italic pt-2 pb-2">Me encantó la propuesta , la recomiendo. Ideal para usarlo en casa con la familia, y como herramienta en la parte educativa...</p>
                         <i className="fas fa-star text-yellow-500 ml-1 mt-2"></i>
                         <i className="fas fa-star text-yellow-500 ml-1"></i>
                         <i className="fas fa-star text-yellow-500 ml-1"></i>
                         <i className="fas fa-star text-yellow-500 ml-1"></i>
                         <i className="fas fa-star text-yellow-500 ml-1 mb-2"></i>
-                        <p className="font-Autor font-semibold text-sm text-gray-900 tracking-wide mt-2">{comentario.author}</p>
-                        <p className="font-Fuente text-sm text-gray-500 tracking-wide">{comentario.source}</p>
+                        <p className="font-Autor font-semibold text-sm text-gray-900 tracking-wide mt-2">Paola Rossi</p>
+                        <p className="font-Fuente text-sm text-gray-500 tracking-wide">mediante Play Store</p>
                     </div>
-                ))}
+                    <div className="comentario bg-white p-5 m-5 rounded-3xl border-l-8 border-sky-200 ">
+                        <i className="fas fa-quote-right text-gray-300 text-4xl"></i>
+                        <h4 className="text-2xl mt-2">Una herramienta muy valiosa</h4>
+                        <p className="text-base text-gray-500 italic pt-2 pb-2">Excelente juego, super recomendable!!! como padres y madres es una guía bárbara para trabajar mindfulness con nuestros hijos…</p>
+                        <i className="fas fa-star text-yellow-500 ml-1 mt-2"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1 mb-2"></i>
+                        <p className="font-Autor font-semibold text-sm text-gray-900 tracking-wide mt-2">Maria Noel Martínez</p>
+                        <p className="font-Fuente text-sm text-gray-500 tracking-wide">mediante Play Store</p>
+                    </div>
+                    <div className="comentario bg-white p-5 m-5 rounded-3xl border-l-8 border-sky-200 mx-4">
+                        <i className="fas fa-quote-right text-gray-300 text-4xl"></i>
+                        <h4 className="text-2xl mt-2">Muy Buena!</h4>
+                        <p className="text-base text-gray-500 italic pt-2 pb-2">Es un buena oportunidad de que niños (y adultos también) aprendan a darse un tiempo para escucharse y 'leer' cómo se sienten…</p>
+                        <i className="fas fa-star text-yellow-500 ml-1 mt-2"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1 mb-2"></i>
+                        <p className="font-Autor font-semibold text-sm text-gray-900 tracking-wide mt-2">Belen Cacere</p>
+                        <p className="font-Fuente text-sm text-gray-500 tracking-wide">mediante Play Store</p>
+                    </div>
+                    <div className="comentario bg-white p-5 m-5 rounded-3xl border-l-8 border-sky-200 ">
+                        <i className="fas fa-quote-right text-gray-300 text-4xl"></i>
+                        <h4 className="text-2xl mt-2">Exelente!</h4>
+                        <p className="text-base text-gray-500 italic pt-2 pb-2">Excelente la app! Me encontré con muchos recursos para conocer más a mi hija y herramientas para ella…</p>
+                        <i className="fas fa-star text-yellow-500 ml-1 mt-2"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1"></i>
+                        <i className="fas fa-star text-yellow-500 ml-1 mb-2"></i>
+                        <p className="font-Autor font-semibold text-sm text-gray-900 tracking-wide mt-2">Rosario Moraco</p>
+                        <p className="font-Fuente text-sm text-gray-500 tracking-wide">mediante Play Store</p>
+                    </div>
+                </Slider>
             </div>
-            <div id="Dots" className="justify-center flex space-x-1  text-dots">
-                {Array.from({ length: comentarios.length }).map((_, index) => (
-                    <span
-                        key={index}
-                        className={`fas fa-circle cursor-pointer ${index === currentIndex ? 'text-blue-500' : 'text-fondo'}`}
-                        onClick={() => handleDotClick(index)}
-                    ></span>
-                ))}
-            </div>
+            <br /><br />
         </div>
     );
-}
-
+};
 
 export default Comentarios;
